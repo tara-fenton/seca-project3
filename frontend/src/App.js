12bae5b43d47a820e8decdeeb961da35191a7c64
Tara Fenton <tarafenton@ool-ad02048c.dyn.optonline.net>
2018-05-31 12:30:52 -0400

    create user component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserComponent from './components/UserComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-hellow wrokd
+        <UserComponent />
       </div>
     );
   }
---
+++ frontend/src/components/UserComponent/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserComponent extends Component {
+  render() {
+    return (
+      <div>
+        user Component
+      </div>
+    );
+  }
+}
+
+export default UserComponent;
